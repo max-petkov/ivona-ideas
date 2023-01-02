57eb312bb1b19e825171c8c9bb8bbded27f52906
Maximilian Petkov <max@stan.vision>
2023-01-02 16:49:21 +0200

    Added Hero Image Animation

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 gsap.registerPlugin(MotionPathPlugin);
 
+toggleMenu();
 neuralLinks();
+heroAnimation();
 
 function neuralLinks() {
   const links = document.querySelectorAll(".neural-links__svg .link");
@@ -63,13 +65,12 @@ function neuralLinks() {
 
   window.addEventListener("resize", neuralLinks);
 
-
   function animate(config) {
     const orb = document.querySelector(config.orb);
     const link = config.link;
     gsap.fromTo(
       orb,
-      { opacity: 1,},
+      { opacity: 1 },
       {
         onStart: () => orb.classList.remove("is-hidden"),
         delay: config.delay,
@@ -127,11 +128,11 @@ function neuralLinks() {
       card.bottomCenter.scrollWidth / 2 +
       "px";
 
-      card.topRight.classList.remove("is-hidden");
-      card.topLeft.classList.remove("is-hidden");
-      card.bottomRight.classList.remove("is-hidden");
-      card.bottomLeft.classList.remove("is-hidden");
-      card.bottomCenter.classList.remove("is-hidden");
+    card.topRight.classList.remove("is-hidden");
+    card.topLeft.classList.remove("is-hidden");
+    card.bottomRight.classList.remove("is-hidden");
+    card.bottomLeft.classList.remove("is-hidden");
+    card.bottomCenter.classList.remove("is-hidden");
   }
 
   function getPosition(el) {
@@ -145,4 +146,96 @@ function neuralLinks() {
       right: right + scrollX,
     };
   }
+}
+
+function heroAnimation() {
+  const hero = document.querySelector(".hero");
+  const mouse = {
+    x: 0,
+    y: 0,
+  };
+  const shape = {
+    el: hero.querySelector(".hero__svg-shape"),
+    x: mouse.x,
+    y: mouse.y,
+  };
+  const model = {
+    el: hero.querySelector(".hero__svg-model"),
+    x: mouse.x,
+    defaultX: 277,
+  };
+  let isAnimating = true;
+
+  hero.addEventListener("mouseenter", function () {
+    isAnimating = true;
+    animate();
+  });
+
+  hero.addEventListener("mouseleave", function () {
+    isAnimating = false;
+  });
+
+  hero.addEventListener("mousemove", function (e) {
+    if (!isAnimating) return;
+
+    const { height, width } = this.getBoundingClientRect();
+    mouse.x = (e.offsetX / width - 0.5) * 30;
+    mouse.y = (e.offsetY / height - 0.5) * 40;
+  });
+
+  function animate() {
+    shape.x = lerp(shape.x, mouse.x, 0.05);
+    shape.y = lerp(shape.y, mouse.y, 0.05);
+    model.x = lerp(model.x / 1.09, mouse.x, 0.01);
+
+    shape.el.style.transform = `rotateX(${shape.x.toFixed(4)}deg) rotateY(${shape.y.toFixed(4)}deg)`;
+    model.el.setAttribute("x", model.x + model.defaultX);
+
+    if (!isAnimating) {
+      cancelAnimationFrame(animate);
+      return;
+    }
+
+    requestAnimationFrame(animate);
+  }
+
+  function lerp(start, end, transition) {
+    return (1 - transition) * start + transition * end;
+  }
+}
+
+function toggleMenu() {
+  const nav = document.querySelector("nav");
+  const { width, height } = nav.getBoundingClientRect();
+  const navTitle = nav.querySelector("p");
+  const paddingY = 16;
+  const paddingX = 24;
+
+  nav.style.setProperty("--h-nav", height + "px");
+  nav.style.setProperty("--w-nav", width.toFixed(2) + "px");
+
+  nav.addEventListener("click", function (e) {
+    const isCloseBtn = e.target.classList.contains("nav__btn-close");
+    const isClosed = this.classList.contains("is-closed");
+
+    if (isCloseBtn) {
+      nav.style.setProperty(
+        "--h-nav",
+        paddingY * 2 + navTitle.scrollHeight + "px"
+      );
+      nav.style.setProperty(
+        "--w-nav",
+        paddingX * 2 + navTitle.scrollWidth + "px"
+      );
+      nav.classList.add("is-closed");
+
+      return;
+    }
+
+    if (isClosed) {
+      this.style.setProperty("--h-nav", height + "px");
+      this.style.setProperty("--w-nav", width.toFixed(2) + "px");
+      this.classList.remove("is-closed");
+    }
+  });
 }
